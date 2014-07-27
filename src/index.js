7f2309f5a348bd8c6387a381aacc1ebc84c17cd0
Samy Pessé <samypesse@gmail.com>
2014-07-27 15:42:54 -0700

    Add command "about.welcome"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-define(function() {
+define([
+    "text!src/welcome.md"
+], function(welcomeMessage) {
     var commands = codebox.require("core/commands");
     var dialogs = codebox.require("utils/dialogs");
+    var File = codebox.require("models/file");
 
+    // About dialog
     commands.register({
         id: "about.show",
-        title: "About",
+        title: "Application: About",
         shortcuts: [
             "mod+shift+a"
         ],
@@ -12,4 +16,15 @@ define(function() {
             return dialogs.alert("About Codebox");
         }
     });
-});
+
+    // Welcome message
+    commands.register({
+        id: "about.welcome",
+        title: "Application: Welcome",
+        run: function() {
+            return commands.run("file.open", {
+                file: File.buffer("welcome.md", welcomeMessage)
+            })
+        }
+    });
+});a
