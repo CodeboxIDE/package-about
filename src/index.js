3cc9994be2beb5bdccc677c928bff7e974ea0239
Samy Pessé <samypesse@gmail.com>
2014-07-27 16:16:39 -0700

    Add command to read release notes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 define([
     "text!src/welcome.md"
 ], function(welcomeMessage) {
+    var _ = codebox.require("hr/utils");
     var commands = codebox.require("core/commands");
     var dialogs = codebox.require("utils/dialogs");
     var File = codebox.require("models/file");
+    var rpc = codebox.require("core/rpc");
+
+    var about = _.memoize(rpc.execute.bind(rpc, "codebox/about"));
+    var releasesNotes = _.memoize(rpc.execute.bind(rpc, "codebox/changes"));
 
     // About dialog
     commands.register({
         id: "about.show",
         title: "Application: About",
         run: function() {
-            return dialogs.alert("About Codebox");
+            return about()
+            .then(function(pkg) {
+                return dialogs.alert("Codebox version: "+pkg.version);
+            });
         }
     });
 
@@ -24,4 +32,22 @@ define([
             })
         }
     });
-});a
+
+    // Releases notes
+    commands.register({
+        id: "about.releases",
+        title: "Application: Releases Notes",
+        run: function() {
+            return releasesNotes()
+            .get("content")
+            .then(function(content) {
+                return commands.run("file.open", {
+                    file: File.buffer("Releases Notes.md", content)
+                })
+            });
+        }
+    });
+
+    // Put in cache version
+    about();
+});
